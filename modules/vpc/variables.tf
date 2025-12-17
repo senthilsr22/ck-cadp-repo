2c074060ec6084e78eeaedc9a79f760fdcfbf141
Senthil Kumar <senthisr@gmail.com>
2025-12-17 13:36:37 +0530

    Added multiple files

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr" {}
+
+variable "public_subnets" {}
+
+variable "private_subnets" {}
+
+variable "availability_zones" {}
+
+variable "project_name" {}
+
+variable "environment" {}
